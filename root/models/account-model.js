5dfbbcb80a5aaab13f86f178e9cf4ce17061982e
jiaxx <yujia.eng.2025@smu.edu.sg>
2026-03-28 13:08:22 +0800

    minor change
    Profile -> Edit Profile

--- root/models/account-model.js
+++ root/models/account-model.js
@@ -4,12 +4,23 @@ const mongoose = require('mongoose');
 const AccountSchema = new mongoose.Schema({
     username: {
         type: String,
-        required: [true, 'A user must have a username']
+        required: [true, 'A user must have a username'],
+        unique: true
     },
     email: {
         type: String,
         required: [true, 'A user must have a password'],
-        unique: true
+    
+    },
+    
+    bio:{
+        type: String,
+        required: false
+    },
+    profilepic:{
+        type: String,
+        default: '../images/pfp/defaultpfp.jpg'
+
     },
     password: {
         type: String,
@@ -37,12 +48,24 @@ exports.findByEmail = function(email) {
 };
 
 // Update profile by email
-exports.updateUser = function(email, newUsername, newEmail) {
-    return Account.updateOne({email: email}, {username: newUsername, email: newEmail});
+exports.updateUser = function(email, newUsername, newEmail, newbio, newpfp) {
+    return Account.updateOne({email: email}, {username: newUsername, email: newEmail, bio: newbio, profilepic:newpfp});
 };
 
 // Update profile password by email
 exports.updatePassword = function(email, newPassword) {
     return Account.updateOne({email: email}, {password: newPassword});
 };
+
+//allow users to delete their account
+exports.deleteacct = function (email) {
+    return Account.findOneAndDelete({email: email});
+};
+
+//find account through id
+exports.findByID = function (id) {
+    return Account.findById(id);
+};
+
+
 
